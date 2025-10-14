05f7412db73a4da4c37c89570b63429a0012efa2
Michael <misha.h@woof.software>
2025-10-14 17:40:09 +0200

    feat(capo): add email notification when price exceeds CAPO by 10%
    - Implemented delta check (|price - capo|) with 10% threshold
    - Sends warning email when price > CAPO + 10%

---
+++ src/common/types/capo-values.ts
@@ -0,0 +1,8 @@
+export interface CapoValues {
+  currentRatio: string;
+  maxRatio: string;
+  currentGrowthRate: number;
+  maxGrowthRate: number;
+  isCapped: boolean;
+  utilizationPercent: number;
+}
