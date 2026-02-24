b5f35f002ef31b87cf5e54f57a1f10b29f9da716
denis-st-woof-software <denis.st@woof.software>
2026-02-24 12:51:41 +0200

    fix(source-update): forFeature deleted

--- src/modules/sources-update/sources-update.module.ts
+++ src/modules/sources-update/sources-update.module.ts
@@ -4,9 +4,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AssetEntity } from 'modules/asset/asset.entity';
 import { SourceEntity } from 'modules/source/source.entity';
 import { NetworkModule } from 'modules/network/network.module';
-import { ReserveEntity, IncomesEntity, SpendsEntity } from 'modules/history/entities';
-import { TreasuryEntity } from 'modules/treasury/treasury.entity';
-import { RevenueEntity } from 'modules/revenue/revenue.entity';
 
 import { SyncRepository } from './repositories/sync.repository';
 import { SourcesUpdateService } from './sources-update.service';
@@ -14,18 +11,7 @@ import { SourcesUpdateCommand } from './cli/sources-update.command';
 import { SourcesUpdateValidationService } from './sources-validator';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      AssetEntity,
-      SourceEntity,
-      ReserveEntity,
-      IncomesEntity,
-      SpendsEntity,
-      TreasuryEntity,
-      RevenueEntity,
-    ]),
-    NetworkModule,
-  ],
+  imports: [TypeOrmModule.forFeature([AssetEntity, SourceEntity]), NetworkModule],
   providers: [
     SyncRepository,
     SourcesUpdateService,
