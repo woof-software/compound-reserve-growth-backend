490ee3758974e7616c8dea64fc4e861eb1e8beae
denis-st-woof-software <denis.st@woof.software>
2026-03-31 11:00:48 +0300

    fix(common): fix pagination

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -2,8 +2,8 @@ import { Order } from '@/common/enum/order.enum';
 
 export class PaginationDto {
   constructor(
-    public page?: number,
-    public perPage?: number,
+    public page: number,
+    public perPage: number,
     public order?: Order,
     public search?: string,
   ) {}
