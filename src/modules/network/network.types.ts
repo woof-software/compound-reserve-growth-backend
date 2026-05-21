cd904df81654296195ec0de63cc2ff23f6c788f6
Stanislav <200855642+StasWoof@users.noreply.github.com>
2026-05-21 14:51:37 +0200

    Update networks.config.ts

--- src/modules/network/network.types.ts
+++ src/modules/network/network.types.ts
@@ -1,5 +1,10 @@
+export type QuoteUsdFeedSymbol = 'ETH' | 'BTC' | 'RON';
+
 export interface NetworkConfig {
   network: string;
   chainId: number;
   url: string;
+  batchMaxCount?: number;
+  quoteUsdFeeds?: Partial<Record<QuoteUsdFeedSymbol, string>>;
+  wstEthAddress?: string;
 }
